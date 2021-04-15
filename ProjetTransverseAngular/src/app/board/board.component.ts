d7e8e209e2828b070f5b9be3d2b9a9f365eb3ee2
root <root@DESKTOP-HN2V8RO.localdomain>
2021-04-15 14:54:13 +0200

    Ajout de la lecture de la base de données

--- ProjetTransverseAngular/src/app/board/board.component.ts
+++ ProjetTransverseAngular/src/app/board/board.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
+
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
@@ -7,18 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
   squares: any[];
-  nbr_cases: number;
+  nbrCases: number;
+  levelData : any ;
+  actualLevel: number;
 
 
-  constructor() {
+  constructor(private httpClient: HttpClient){
     this.squares = Array(0).fill(null);
-    this.nbr_cases = 0;
+    this.nbrCases = 0;
+    this.actualLevel = 1;
   }
 
   ngOnInit(): void {
+    this.newGame()
+  }
+
+  getGame(){
+    this.httpClient.get("http://localhost:3000/allLevel", { responseType: 'text' }).subscribe(res => {this.levelData = JSON.parse(res);console.log(res);});
+
+    //console.log(this.levelData["Lenght"]);
   }
 
   newGame(){
+    this.getGame()
+
+    //console.log(this.levelData["Lenght"]);
+
     this.squares = Array(30 * 20).fill('empty_square.png');
     for (let i = 0; i < 30; i++) {
       this.squares[i] = 'square.png'
@@ -29,7 +46,21 @@ export class BoardComponent implements OnInit {
       this.squares[i * 30 + 29] = 'square.png'
     }
 
-    this.nbr_cases = 30 * 20;
+    this.nbrCases = 30 * 20;
+  }
+
+  nextLevel(){
+    for (let i = 1; i < 19; i++) {
+      this.squares[5+i*30] = 'square.png'
+      this.squares[7+i*30] = 'empty_square.png'
+    }
+  }
+
+  lastLevel(){
+    for (let i = 1; i < 19; i++) {
+      this.squares[7+i*30] = 'square.png'
+      this.squares[5+i*30] = 'empty_square.png'
+    }
   }
 
 }
