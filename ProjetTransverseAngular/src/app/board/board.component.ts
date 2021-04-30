7fa8fdd485626279e86d6ee188f56054aa18d4c0
root <root@DESKTOP-HN2V8RO.localdomain>
2021-04-30 16:36:25 +0200

    Ajout de 2 niveaux ainsi que d'un bouton niveau suivant et niveau précedent, début d'un systeme pour communiquer entre le parent et l'enfant

--- ProjetTransverseAngular/src/app/board/board.component.ts
+++ ProjetTransverseAngular/src/app/board/board.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MapComponent } from '../map/map.component';
+import { Output, EventEmitter } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
 
@@ -9,16 +11,19 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./board.component.css']
 })
 export class BoardComponent implements OnInit {
+
   squares: any[];
   nbrCases: number;
   levelData : any;
   actualLevel: number;
+  newLevel: string;
 
 
   constructor(private httpClient: HttpClient){
     this.squares = Array(0).fill(null);
     this.nbrCases = 0;
     this.actualLevel = 1;
+    this.newLevel = " ";
   }
 
   ngOnInit(): void {
@@ -27,7 +32,8 @@ export class BoardComponent implements OnInit {
 
   newGame(){
 
-    this.squares = Array(this.levelData[this.actualLevel-1].Lenght * this.levelData[this.actualLevel-1].Width).fill('empty_square.png');
+    this.refreshLevel();
+    /*
     for (let i = 0; i < this.levelData[this.actualLevel-1].Lenght; i++) {
       this.squares[i] = 'square.png'
       this.squares[(this.levelData[this.actualLevel-1].Width - 1 ) * this.levelData[this.actualLevel-1].Lenght + i] = 'square.png'
@@ -35,21 +41,38 @@ export class BoardComponent implements OnInit {
     for (let i = 1; i < this.levelData[this.actualLevel-1].Width; i++) {
       this.squares[i * this.levelData[this.actualLevel-1].Lenght] = 'square.png'
       this.squares[i * this.levelData[this.actualLevel-1].Lenght + this.levelData[this.actualLevel-1].Lenght - 1] = 'square.png'
-    }
+    }*/
+
+  }
+
+  refreshLevel(){
+    this.squares = Array(this.levelData[this.actualLevel-1].Lenght * this.levelData[this.actualLevel-1].Width).fill('empty_square.png');
+
+    this.newLevel = this.levelData[this.actualLevel-1].Container;
 
     this.nbrCases = this.levelData[this.actualLevel-1].Lenght * this.levelData[this.actualLevel-1].Width;
+    for(let i = 0; i < this.nbrCases; i++){
+      if(this.newLevel[i] == '0'){
+        this.squares[i] = 'empty_square.png'
+      }
+      if(this.newLevel[i] == '1'){
+        this.squares[i] = 'square.png'
+      }
+    }
   }
 
-  actualiseLevel(){
-
-  }
+  //@Output() nextLevelInStyle = new EventEmitter<string>();
 
   nextLevel(){
+    //this.nextLevelInStyle.emit("this.actualLevel+1");
+    this.actualLevel += 1;
+    this.refreshLevel();
 
   }
 
   lastLevel(){
-
+    this.actualLevel -= 1;
+    this.refreshLevel();
   }
 
 }
--- ProjetTransverseAngular/src/app/map/map.component.ts
+++ ProjetTransverseAngular/src/app/map/map.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BoardComponent } from '../board/board.component';
+
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-map',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  levelData : any;
+  actualLevel: number;
+  lenght: number;
+  startGame: number;
 
-  ngOnInit(): void {
+
+  constructor(private httpClient: HttpClient) {
+    this.actualLevel = 1;
+    this.lenght = 0;
+    this.startGame = 0;
   }
 
+  ngOnInit(): void {
+    this.httpClient.get("http://localhost:3000/allLevel", { responseType: 'text' }).subscribe(res => {this.levelData = JSON.parse(res);});
+  }
+
+  startGameOnMenu(){
+    this.startGame = 1;
+  }
+
+  nextLevelStyle(lvl: string){
+    console.log("test");
+    this.actualLevel += 1;
+    //this.startGame = 1;
+  }
 }
